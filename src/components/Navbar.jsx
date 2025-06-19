803d5fee0bc5f9b45820f985ce1b7762dbc057ba
Inder <kumar.inder.com>
2025-06-19 09:41:18 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,9 +17,11 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-white shadow-md sticky top-0 z-50 transition-all duration-300">
+    // <nav className="bg-white shadow-md sticky top-0 z-50 transition-all duration-500">
+      <nav className="bg-white shadow-md sticky top-0 z-50 h-14 transition-all duration-500">
+
       <div className="max-w-7xl mx-auto px-4">
-        <div className="flex justify-between items-center min-h-[5rem] py-3">
+        <div className="flex justify-between items-center h-full">
           {/* Left: Logo + Flags (horizontal) */}
           <div className="flex items-center space-x-4">
             {/* Logo */}
